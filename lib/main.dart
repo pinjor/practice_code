ce2d2e97cb91eeb32e5a7779183896fe0b17a98a
Md.Masfakuzzaman <pinjor033@gmail.com>
2024-08-14 23:58:41 +0600

    all code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:practice_one/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //debugShowCheckedModeBanner: false,
-        home: Home());
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Text Styling App'),
+        ),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const ListTile(
+                    title: Text(
+                      'Flutter Text Styling',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 25.0),
+                    ),
+                    subtitle: Text(
+                      'Experiment with text styles',
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic, fontSize: 16.0),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        showSnackbar(context, 'You clicked the button!');
+                      },
+                      child: Text('Click Me'))
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
